294f3de9fa46b6853537d8669bd64bc6dbf93d66
franckf <efydd.artio@tuta.io>
2020-11-28 16:29:28 +0100

    [TEST] (route): /users tests with supertest

--- setup/ts-node-express/src/__tests__/routes_users.test.ts
+++ setup/ts-node-express/src/__tests__/routes_users.test.ts
@@ -1,1 +1,11 @@
-test.todo('Some test I still need to do')
+import request from 'supertest';
+import app from './../app';
+
+test("basic route users", async () => {
+    const response = await request(app).get('/users')
+    expect(response.status).toBe(200)
+    expect(response.status).not.toBe(404)
+    expect(response.type).toBe('text/html')
+    expect(response.headers).toBeDefined()
+    expect(response.text).toBe('respond with a resource')
+})
