9201761e7a1e31ae1d338aaedf998e51d47670e8
franckf <efydd.artio@tuta.io>
2020-11-28 17:11:37 +0100

    [TEST] (route) /home test for render page

--- setup/ts-node-express/src/__tests__/routes_index.test.ts
+++ setup/ts-node-express/src/__tests__/routes_index.test.ts
@@ -1,1 +1,11 @@
-test.todo('Some test I still need to do')
+import request from 'supertest';
+import app from './../app';
+
+test("basic route home", async () => {
+    const response = await request(app).get('/')
+    expect(response.status).toBe(200)
+    expect(response.status).not.toBe(404)
+    expect(response.type).toBe('text/html')
+    expect(response.headers).toBeDefined()
+    expect(response.text).toMatch(/Express/)
+})
