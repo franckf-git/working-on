88aae70e22fd55efb2559250601529c3f87ddf9e
franckf <efydd.artio@tuta.io>
2020-11-28 16:45:15 +0100

    [TEST] (route): /404 test if a 404 is display

--- setup/ts-node-express/src/__tests__/app.test.ts
+++ setup/ts-node-express/src/__tests__/app.test.ts
@@ -1,1 +1,10 @@
-test.todo('Some test I still need to do')
+import request from 'supertest';
+import app from './../app';
+
+test("if a 404 if displayed on unknown pages", async () => {
+    const response = await request(app).get('/arandompagewithoutroute')
+    expect(response.status).toBe(404)
+    expect(response.type).toBe('text/html')
+    expect(response.headers).toBeDefined()
+    expect(response.text).toMatch(/Not.Found/)
+})
