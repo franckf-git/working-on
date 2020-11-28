70ad1a7af1f0e0090401c99366214845ea8aeb6e
franckf <efydd.artio@tuta.io>
2020-11-28 17:29:16 +0100

    [REFACTO] (bin): /www var > const

--- setup/ts-node-express/src/bin/www.ts
+++ setup/ts-node-express/src/bin/www.ts
@@ -9,14 +9,14 @@ import http from 'http'
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000')
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app)
+const server = http.createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -31,7 +31,7 @@ server.on('listening', onListening)
  */
 
 function normalizePort(val: any) {
-  var port = parseInt(val, 10)
+  const port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
@@ -55,7 +55,7 @@ function onError(error: any) {
     throw error
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port
 
@@ -79,8 +79,8 @@ function onError(error: any) {
  */
 
 function onListening() {
-  var addr = server.address()
-  var bind = typeof addr === 'string'
+  const addr = server.address()
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + port
   console.log('Listening on ' + bind)
