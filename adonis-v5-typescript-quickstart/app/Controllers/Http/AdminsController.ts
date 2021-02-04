50273c24a563b3eacfe0b3af97dbf09ac1aa7bd6
franckf <>
2021-02-04 06:21:28 +0100

    feat: validate data

--- adonis-v5-typescript-quickstart/app/Controllers/Http/AdminsController.ts
+++ adonis-v5-typescript-quickstart/app/Controllers/Http/AdminsController.ts
@@ -1,15 +1,17 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
 import { Request } from "@adonisjs/core/build/standalone";
 import View from "@ioc:Adonis/Core/View";
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, validator } from '@ioc:Adonis/Core/Validator'
 
 export default class AdminsController {
     public async index() {
         return View.render('admin')
     }
     public async connect({ request }: HttpContextContract) {
-        console.log(request.all())
+        const postSchema = schema.create({ username: schema.string(), password: schema.string() })
+        const data = await request.validate({ schema: postSchema, cacheKey: request.url() })
+
+        console.log(data)
         return 'connect admin'
     }
 }
