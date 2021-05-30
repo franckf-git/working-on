c3b173c01f1beb2a63ddf4c687cb0fd6e382f73a
franckf <>
2021-05-30 06:28:25 +0200

    chores: more todos

--- conky-server-go/main.go
+++ conky-server-go/main.go
@@ -14,9 +14,10 @@ var commands map[string]string = map[string]string{
 	"user":     "whoami",
 	"ips":      "ip a",
 	"hostname": "hostname",
-	"packages": "dnf list --installed | wc -l", // prévoir pour debian distro
+	"packages": "dnf list --installed | wc -l", // TODO prévoir pour debian distro
 	"kernel":   "uname -a",
 	// "os":       "lsb_release -a",
+	// TODO ignorer les commandes inconnues - mais mettre une alerte
 	"top":    "ps aux | sort -nk +4 | tail",
 	"memory": "free -h",
 	"loads":  "uptime | cut -d' ' -f10-",
@@ -29,16 +30,18 @@ func main() {
 	log.Println("Le serveur est en ligne, visitez http://127.0.0.1:5500")
 	// TODO ouverture auto du navigateur par défaut ? `xdg-open http://127.0.0.1:5500`
 	http.ListenAndServe(":5500", nil)
-	return
 }
 
 func serveTemplate(res http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+
 	var commandsExec map[string]string
 	for title, command := range commands {
 		commandsExec[title] = runCommand(command)
 	}
 
 	indextemplate, err := template.ParseFiles("indextemplate.html")
+	// TODO include html file to only have one file
 	if err != nil {
 		panic(err)
 	}
