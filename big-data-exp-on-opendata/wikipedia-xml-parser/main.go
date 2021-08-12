ffa666acbb810c5f8855dab1f988a852e12c8757
franckf <>
2021-08-12 15:33:13 +0200

    feat: prototype/try to xml parsing

--- big-data-exp-on-opendata/wikipedia-xml-parser/main.go
+++ big-data-exp-on-opendata/wikipedia-xml-parser/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"encoding/xml"
 	"log"
+	"strings"
 )
+
+type feed struct {
+	Doc []Doc `xml:"doc"`
+}
+
+type Doc struct {
+	Title    string `xml:"title"`
+	Url      string `xml:"url"`
+	Abstract string `xml:"abstract"`
+	Links    string `xml:"links"`
+}
 
 func main() {
 	db := openDatabase()
 	defer db.Close()
-
 	startDatabase(db)
-	insertDatabase(db, 8, "title", "https://url.test", "abstract", 2)
-	insertDatabase(db, 9, "dex", "url", "abstract", 54)
-	insertDatabase(db, 11, "TITLE", "https://url.fr", "abstract4", 462)
-	output := selectAllDatabase(db)
-	log.Println("Results:", output)
+
+	data := `
+	<feed>
+	<doc>
+	<title>Wikipedia: Mariapia Degli Esposti</title>
+	<url>https://en.wikipedia.org/wiki/Mariapia_Degli_Esposti</url>
+	<abstract>Cancer Council of Western Australia Cancer Researcher of the Year 2017Research Excellence Awards - Cancer Council Western Australia (2017)</abstract>
+	<links>
+	<sublink linktype="nav"><anchor>Research</anchor><link>https://en.wikipedia.org/wiki/Mariapia_Degli_Esposti#Research</link></sublink>
+	<sublink linktype="nav"><anchor>Education</anchor><link>https://en.wikipedia.org/wiki/Mariapia_Degli_Esposti#Education</link></sublink>
+	</links>
+	</doc>
+	</feed>
+	`
+	mariapia := &feed{}
+	xml.Unmarshal([]byte(data), mariapia)
+
+	log.Printf("%#v\n", mariapia)
+	log.Println("title:", mariapia.Doc[0].Title)
+	log.Println("url:", mariapia.Doc[0].Url)
+	log.Println("abstract:", mariapia.Doc[0].Abstract)
+	log.Println("links:", len(strings.Split(mariapia.Doc[0].Links, "\n"))/2)
+
 }
