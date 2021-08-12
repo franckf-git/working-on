82c2213e60d9634e1ab6dd736f9c6bc412eeb604
franckf <>
2021-08-12 13:19:54 +0200

    test: add tests to return all values

--- big-data-exp-on-opendata/wikipedia-xml-parser/model_test.go
+++ big-data-exp-on-opendata/wikipedia-xml-parser/model_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"reflect"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -22,6 +23,25 @@ func Test_insertDatabase(t *testing.T) {
 	falseInsert := insertDatabase(db, 7, "title2", "https://url.test", "abstract", 2)
 	if !firstInsert && !secondInsert && falseInsert {
 		t.Errorf("insertDatabase tests fail")
+	}
+
+	os.Remove("./test.db")
+}
+
+func Test_selectAllDatabase(t *testing.T) {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal("Fail to open database:", err)
+	}
+	defer db.Close()
+	startDatabase(db)
+
+	insertDatabase(db, 8, "title1", "https://url.test", "abstract", 2)
+	insertDatabase(db, 7, "title2", "https://url.test", "abstract", 2)
+	want := [][]string{{"7", "title2", "https://url.test", "abstract", "2"}, {"8", "title1", "https://url.test", "abstract", "2"}}
+	got := selectAllDatabase(db)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("selectAllDatabasetests fail: want %v - got %v", want, got)
 	}
 
 	os.Remove("./test.db")
