8f6573e78a9dbdd8263d94d264402b90333f4169
franckf <>
2021-08-14 15:13:18 +0200

    feat: finish createChart func to accept map of letters

--- big-data-exp-on-opendata/wikipedia-xml-parser/visualdata.go
+++ big-data-exp-on-opendata/wikipedia-xml-parser/visualdata.go
@@ -7,17 +7,23 @@ import (
 )
 
 func main() {
-	createChart()
+	var datas = map[string]float64{
+		"A": 25,
+		"B": 36,
+		"C": 0,
+		"D": 8,
+	}
+	createChart(datas)
 }
 
-func createChart() {
+func createChart(datas map[string]float64) {
 
 	values := []chart.Value{}
 
-	adding1 := []chart.Value{{Value: 50, Label: "Encore"}}
-	adding2 := []chart.Value{{Value: 10, Label: "Avant"}}
-	values = append(values, adding1...)
-	values = append(values, adding2...)
+	for label, value := range datas {
+		adding := []chart.Value{{Label: label, Value: value}}
+		values = append(values, adding...)
+	}
 
 	graph := chart.BarChart{
 		Title: "Stats - Links by Letter",
