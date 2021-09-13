6429ea686fb61ab641b5306ccd149d490b53d468
franckf <>
2021-09-13 07:24:01 +0200

    feat: add config file from variables

---
+++ api-in-golang/config/config.go
@@ -0,0 +1,4 @@
+package config
+
+var PORT string = ":8000"
+var WelcomeMessage string = "Welcome, to have more details about this API, visit /api/v1/docs"
--- api-in-golang/main.go
+++ api-in-golang/main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 
+	"lite-api-crud/config"
+
 	"github.com/gorilla/mux"
 )
 
 func WelcomePage(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "welcome")
+	fmt.Fprintln(res, config.WelcomeMessage)
 }
 
 func ShowAllPosts(res http.ResponseWriter, req *http.Request) {
@@ -20,5 +22,5 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", WelcomePage)
 	router.HandleFunc("/api/v1/posts", ShowAllPosts)
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(config.PORT, router))
 }
