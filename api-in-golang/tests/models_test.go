e8d3a67b8ade7f2a8d2f3418f2d05f50ff18eb33
franckf <>
2021-09-18 08:13:26 +0200

    test: refacto models tests

--- api-in-golang/tests/models_test.go
+++ api-in-golang/tests/models_test.go
@@ -1,10 +1,8 @@
 package tests
 
 import (
-	"database/sql"
 	"lite-api-crud/config"
 	"lite-api-crud/models"
-	"log"
 	"os"
 	"reflect"
 	"testing"
@@ -14,27 +12,24 @@ import (
 )
 
 func Test_RegisterPost(t *testing.T) {
-	db, err := sql.Open("sqlite3", "./test.db")
-	if err != nil {
-		log.Fatal("Fail to open database:", err)
-	}
+	db := models.OpenDatabase()
 	defer db.Close()
 	models.StartDatabase(db)
+
 	firstInsert, _ := models.RegisterPost(db, "title1", "datas1", 1)
 	secondInsert, _ := models.RegisterPost(db, "title2", "datas2", 2)
 	if firstInsert != 1 && secondInsert != 2 {
 		t.Errorf("RegisterPost tests fail")
 	}
+
 	os.Remove("./test.db")
 }
 
 func Test_GetAllPosts(t *testing.T) {
-	db, err := sql.Open("sqlite3", "./test.db")
-	if err != nil {
-		log.Fatal("Fail to open database:", err)
-	}
+	db := models.OpenDatabase()
 	defer db.Close()
 	models.StartDatabase(db)
+
 	models.RegisterPost(db, "title1", "datas1", 1)
 	models.RegisterPost(db, "title2", "datas2", 2)
 	postsTests := models.GetAllPosts(db)
@@ -48,5 +43,6 @@ func Test_GetAllPosts(t *testing.T) {
 	if !reflect.DeepEqual(postsTests, want) {
 		t.Errorf("GetAllPosts tests fail")
 	}
+
 	os.Remove("./test.db")
 }
