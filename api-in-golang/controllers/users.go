203a96c374cf71cd646ea916940209f327c547d0
franckf <>
2021-09-26 07:32:49 +0200

    feat: add errors mangement in addUser route

--- api-in-golang/controllers/users.go
+++ api-in-golang/controllers/users.go
@@ -11,19 +11,52 @@ import (
 )
 
 func AddUser(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
 	var user models.User
-	json.Unmarshal(body, &user)
+	body, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	w.Header().Set("Content-Type", "application/json")
+
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		log.Println("Error bad content-type formating:", r.Header)
+		failed := config.Message{
+			Status:  "error",
+			Message: "error bad content-type formating:" + fmt.Sprint(r.Header),
+		}
+		res, _ := json.Marshal(failed)
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write(res)
+		return
+	}
+
+	err := json.Unmarshal(body, &user)
+	if err != nil {
+		log.Println("Error decoding user:", err, user)
+		failed := config.Message{
+			Status:  "error",
+			Message: "error while decoding payload " + fmt.Sprint(err, user),
+		}
+		res, _ := json.Marshal(failed)
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write(res)
+		return
+	}
 
 	db := models.OpenDatabase()
 	defer db.Close()
 	id, err := user.RegisterUser(db)
 	if err != nil {
-		log.Printf("err: %#+v\n", err)
+		log.Println("Error register user:", err, user)
+		failed := config.Message{
+			Status:  "error",
+			Message: "error while saving user",
+		}
+		res, _ := json.Marshal(failed)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(res)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
 	successfull := config.Message{
 		Status:  "success",
 		Message: "The user has been saved on id: " + fmt.Sprint(id),
@@ -32,5 +65,4 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	res, _ := json.Marshal(successfull)
 	w.WriteHeader(http.StatusCreated)
 	w.Write(res)
-	return
 }
