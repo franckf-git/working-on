3408cb2313e085b77d2e8832a96c68d36f353212
franckf <>
2021-09-26 16:24:20 +0200

    feat: add email/password validator

--- api-in-golang/controllers/users.go
+++ api-in-golang/controllers/users.go
@@ -8,6 +8,7 @@ import (
 	"lite-api-crud/models"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 func AddUser(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +43,18 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if !CheckEmailPassword(user) {
+		log.Println("Error in email or password validator:", user)
+		failed := config.Message{
+			Status:  "error",
+			Message: "error in email or password validator - email must be a valid email and password must be at least 8 characters, uppercase, lowercase, numbers and specials included",
+		}
+		res, _ := json.Marshal(failed)
+		w.WriteHeader(http.StatusPreconditionRequired)
+		w.Write(res)
+		return
+	}
+
 	db := models.OpenDatabase()
 	defer db.Close()
 	id, err := user.RegisterUser(db)
@@ -65,4 +78,34 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	res, _ := json.Marshal(successfull)
 	w.WriteHeader(http.StatusCreated)
 	w.Write(res)
+}
+
+func CheckEmailPassword(user models.User) bool {
+	var validEmail = regexp.MustCompile(`(?:[a-z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`)
+	if !validEmail.MatchString(user.Email) {
+		return false
+	}
+
+	if len(user.Password) < 8 {
+		return false
+	}
+
+	var numbers = regexp.MustCompile("[0-9]")
+	var lower = regexp.MustCompile("[a-z]")
+	var upper = regexp.MustCompile("[A-Z]")
+	var nospecials = regexp.MustCompile(`[^\w]`)
+
+	if !numbers.MatchString(user.Password) {
+		return false
+	}
+	if !lower.MatchString(user.Password) {
+		return false
+	}
+	if !upper.MatchString(user.Password) {
+		return false
+	}
+	if !nospecials.MatchString(user.Password) {
+		return false
+	}
+	return true
 }
--- api-in-golang/tests/routes_users_test.go
+++ api-in-golang/tests/routes_users_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"lite-api-crud/config"
+	"lite-api-crud/controllers"
+	"lite-api-crud/models"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -45,6 +47,24 @@ func Test_Users_Route(t *testing.T) {
 			contenttype:  "",
 			expectedCode: 415,
 			expectedRes:  `{"status":"error","message":"error while decoding payload <nil> { VERYstrong&Secur3}","id":0}`,
+		},
+		{
+			desc:         "Bad email",
+			route:        "/user",
+			method:       "POST",
+			body:         `{"email":"user1@mail?lan","password":"VERYstrong&Secur3"}`,
+			contenttype:  "",
+			expectedCode: 428,
+			expectedRes:  `{"status":"error","message":"error in email or password validator - email must be a valid email and password must be at least 8 characters, uppercase, lowercase, numbers and specials included","id":0}`,
+		},
+		{
+			desc:         "Bad password",
+			route:        "/user",
+			method:       "POST",
+			body:         `{"email":"user1@mail.lan","password":"VERYstrong&Secur"}`,
+			contenttype:  "",
+			expectedCode: 428,
+			expectedRes:  `{"status":"error","message":"error in email or password validator - email must be a valid email and password must be at least 8 characters, uppercase, lowercase, numbers and specials included","id":0}`,
 		},
 		/*
 			{
@@ -91,6 +111,95 @@ func Test_Users_Route(t *testing.T) {
 			}
 			if gotJSON.Id != expectedJSON.Id {
 				t.Errorf("%v fails, got id: %d", tC.desc, gotJSON.Id)
+			}
+		})
+	}
+}
+
+func Test_checkEmailPassword(t *testing.T) {
+	type args struct {
+		user models.User
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Good mail - Good Password",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "VERYstrong&Secur3",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Invalid email",
+			args: args{
+				user: models.User{
+					Email:    "user1mail.lan",
+					Password: "VERYstrong&Secur3",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Password - bad length",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "rR&4567",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Password - no numbers",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "rR&aaaab",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Password - no uppercase",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "rr&45678",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Password - no lowercase",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "RR&45678",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Password - no special",
+			args: args{
+				user: models.User{
+					Email:    "user1@mail.lan",
+					Password: "rRf45678",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := controllers.CheckEmailPassword(tt.args.user); got != tt.want {
+				t.Errorf("checkEmailPassword() = %v, want %v", got, tt.want)
 			}
 		})
 	}
